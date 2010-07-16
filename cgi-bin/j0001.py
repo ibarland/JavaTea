8e9ef763f86f2255be7d2afb9917571c24381af8
Joel Dominic <jdominic@radford.edu>
2010-07-15 23:44:39 -0400

    First problem example completed and working Adding multiple test cases works Python script for double problem works

--- cgi-bin/j0001.py
+++ cgi-bin/j0001.py
@@ -3,16 +3,24 @@ import cgi
 
 form = cgi.FieldStorage()
 
+def double(init, result):
+    str = "<tr><td>double("+init+") == "+result+"</td>"
+    if int(init)*2 == int(result):
+        return str + "<td>Works</td></tr>"
+    else:
+        return str + "<td>Doesn't work</td></tr>"
+
 print "Content-type:text/html\r\n\r\n"
 print '<html>'
 print '<head>'
-print '<title>Hello Word - First CGI Program</title>'
+print '<title></title>'
 print '</head>'
 print '<body>'
-print form["field1"].value
-print form["result1"].value 
-print form["field2"].value
-print form["result2"].value
-print '<h2>Hello Word! This is my first CGI program</h2>'
+print '<table align="center">'
+a = len(form) / 2
+for i in range(1, a+1):
+    print double(form.getvalue('field'+`i`), form.getvalue('result'+`i`))
+print '</table>'
 print '</body>'
 print '</html>'
+
