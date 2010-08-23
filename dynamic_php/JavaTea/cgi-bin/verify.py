839baa810fad6d0d5bb2faff37d4dbbaaa2aa709
Joel Dominic <jdominic@radford.edu>
2010-08-22 21:05:39 -0400

    Got JPype working

--- dynamic_php/JavaTea/cgi-bin/verify.py
+++ dynamic_php/JavaTea/cgi-bin/verify.py
@@ -3,18 +3,18 @@ import os.path
 from jpype import *
 
 def check(data_list):
-    classpath = os.path.join(os.path.abspath('/home/itec120/'), 'build/classes')
+    classpath = os.path.abspath('.')
     startJVM( getDefaultJVMPath(), "-Djava.class.path=%s" % classpath)
-    InputChecker = JClass('jtea.methods.InputChecker')
+    pack = JPackage('jtea').verify
+    InputChecker = pack.InputChecker
     check = InputChecker()
-    ret = 1
-    for item in data_list:
-        type, data = item
-        if check.isValid(data, type):
-            print "It's all good"
-        else:
-            print "Nope"
+    for case in data_list:
+        for item in case:
+            type, data = item
+            if check.isValid(type, data):
+                print "It's all good"
+            else:
+                print "Nope"
     shutdownJVM()
-    return ret
 
-print check([[(u'int', u'1'), (u'int', u'2')]])
+check([[(u'int', u'1'), (u'int', u'2')]])
